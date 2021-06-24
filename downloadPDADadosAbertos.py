bacf28ab5b8ae3383e0d3c3880d3ecda1c67115f
Jose Ricardo Bettine Pacola (Terceiros) <trc6018760@unimedbh.com.br>
2021-06-24 17:15:30 -0300

    Commit inicial

---
+++ downloadPDADadosAbertos.py
@@ -0,0 +1,41 @@
+import datetime
+import io
+import os
+import zipfile
+import requests
+from bs4 import BeautifulSoup
+
+year = datetime.datetime.now().year
+years = [year-1,year]
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36'}
+url = "http://ftp.dadosabertos.ans.gov.br/FTP/PDA/demonstracoes_contabeis/"
+dirExtract = 'G:\\QLIKVIEW\\SIC\\'
+linksAnos = []
+filesName = []
+
+for y in years:
+	urlAno = url + str(y) + '/'
+	req = requests.get(urlAno, headers = headers)
+	soup = BeautifulSoup(req.content,'html.parser')
+	links = soup.find_all('a')
+	linksAnos += [urlAno + l.get('href') for l in links if l.get('href').endswith('.zip')]
+
+# urlAno = url + str(year) + '/'
+# req = requests.get(urlAno, headers = headers)
+# soup = BeautifulSoup(req.content,'html.parser')
+# links = soup.find_all('a')
+# linksAnos += [urlAno + l.get('href') for l in links if l.get('href').endswith('.zip')]
+
+
+
+for link in linksAnos:
+	fileZip = io.BytesIO(requests.get(link).content)
+	with zipfile.ZipFile(fileZip) as zip_ref:
+		members = zip_ref.namelist()
+		filesName += members
+		zip_ref.extractall(dirExtract)	
+
+
+for file in filesName:
+	os.replace(dirExtract + file, dirExtract + 'FINANCEIRO-' + file)
---
+++ downloadPortalTransparencia.py
@@ -0,0 +1,43 @@
+import zipfile
+import requests
+import io
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from user_agent import generate_user_agent
+from time import sleep
+
+
+urlPortal = 'http://www.portaltransparencia.gov.br/download-de-dados/'
+urls = ["ceis","cepim","cnep"]
+dirExtract = 'G:\\QLIKVIEW\\SIC\\PORTAL'
+
+userAgent = generate_user_agent(device_type="desktop")
+
+options = Options()
+options.add_argument('--headless')
+options.add_argument(f'user-agent={userAgent}')
+options.add_argument('--disable-gpu')
+options.add_argument('--lang=pt_BR')
+# prefs = {"download.default_directory":path}   #path is a string containing the directory you want the downloaded songs stored
+# options.add_experimental_option("prefs",prefs)
+options.add_argument('--no-sandbox')
+options.add_argument("--disable-notifications")
+options.add_argument('--disable-dev-shm-usage')
+
+
+for url in urls:
+
+	driver = webdriver.Chrome(options = options)
+	driver.delete_all_cookies()
+	driver.set_window_size(1061, 701)
+	driver.get(urlPortal + url)
+	sleep(4)
+	link = driver.find_element(By.XPATH,'//*[@id="link-unico"]/li/a').get_attribute('href')
+	print(link)
+	driver.quit()
+	
+	fileZip = io.BytesIO(requests.get(link).content)
+
+	with zipfile.ZipFile(fileZip) as zip_ref:
+		zip_ref.extractall(dirExtract)
